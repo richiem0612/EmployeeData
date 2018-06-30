40b50f4787605b3677a803e3068597c502720590
C.S Wolf <37349534+CSneed7@users.noreply.github.com>
2018-06-30 10:01:55 -0400

    added firebase

---
+++ javascript/data.js
@@ -0,0 +1,13 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBuCEzTY05UQrsBFjSXuz76WcIOwqqQ54Q",
+    authDomain: "employees-a8bfb.firebaseapp.com",
+    databaseURL: "https://employees-a8bfb.firebaseio.com",
+    projectId: "employees-a8bfb",
+    storageBucket: "",
+    messagingSenderId: "1062590078336"
+  };
+  firebase.initializeApp(config);
+
+  var database =  firebase.database();
+  
